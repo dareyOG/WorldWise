9e794a79edec853e40f765afdefd4c9fe18b72bd
Oludire Damilare <oludire@gmail.com>
2024-10-04 17:51:04 +0100

    NEW: return current city data

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,7 @@
-import { useParams, useSearchParams } from 'react-router-dom';
 import styles from './City.module.css';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = date =>
   new Intl.DateTimeFormat('en', {
@@ -12,30 +14,32 @@ const formatDate = date =>
 function City() {
   const { id } = useParams();
 
-  const [searchParams, setSearchParams] = useSearchParams();
+  // const [searchParams, setSearchParams] = useSearchParams();
 
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  // const lat = searchParams.get('lat');
+  // const lng = searchParams.get('lng');
+
+  const { currentCity, getCity } = useCities();
 
   // TEMP DATA
-  const currentCity = {
-    cityName: 'Lisbon',
-    emoji: '🇵🇹',
-    date: '2027-10-31T15:59:59.138Z',
-    notes: 'My favorite city so far!',
-  };
+  // const currentCity = {
+  //   cityName: 'Lisbon',
+  //   emoji: '🇵🇹',
+  //   date: '2027-10-31T15:59:59.138Z',
+  //   notes: 'My favorite city so far!',
+  // };
 
-  const { cityName, emoji, date, notes } = currentCity;
-  return (
-    <>
-      <h1>City {id}</h1>
-      <p>
-        Position: {lat}, {lng}
-      </p>
-    </>
+  // get city
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
   );
 
-  /*  return (
+  const { cityName, emoji, date, notes } = currentCity;
+
+  return (
     <div className={styles.city}>
       <div className={styles.row}>
         <h6>City name</h6>
@@ -67,9 +71,9 @@ function City() {
         </a>
       </div>
 
-      <div><ButtonBack /></div>
+      <div></div>
     </div>
-  ); */
+  );
 }
 
 export default City;
