7e0fe732cc73e23f109b464abbe29e1da5cdaf9e
Oludire Damilare <oludire@gmail.com>
2024-10-07 17:09:38 +0100

    NEW: include Map with leaflet library

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,11 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useState } from 'react';
 
 function Map() {
   const navigate = useNavigate();
+  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
   const [searchParams, setSearchParams] = useSearchParams();
 
@@ -12,14 +15,30 @@ function Map() {
   return (
     <div
       className={styles.mapContainer}
-      onClick={() => {
-        navigate('form');
-      }}
+      // onClick={() => {
+      //   navigate('form');
+      // }}
     >
-      <h1>Map</h1>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      {/* <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
-      </h1>
+      </h1> */}
       {/* <button
         onClick={() => {
           setSearchParams({ lat: 23, lng: 50 });
