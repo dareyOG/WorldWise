77135c06dc80b490997edb9ae7b28cbd2b4710f2
Oludire Damilare <oludire@gmail.com>
2024-10-07 20:26:13 +0100

    NEW: display city markers on map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,22 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import styles from './Map.module.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
+import styles from './Map.module.css';
 
 function Map() {
   const navigate = useNavigate();
-  const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
+  // set map position (lng, lat)
+  const [mapPosition, setMapPosition] = useState([6.465422, 3.406448]);
+
+  // log lat & lng from URL
   const [searchParams, setSearchParams] = useSearchParams();
-
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
+
+  const { cities } = useCities();
+  console.log(cities);
 
   return (
     <div
@@ -29,11 +35,17 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map(city => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
       {/* <h1>Map</h1>
       <h1>
